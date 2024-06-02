88fe9a4ffb02bad2df1fd77bbda6ccc3b6a72ebb
Makhdum <makhdum@gmail.com>
2024-06-02 17:07:53 +0600

    🪟ui: account-page

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -5,6 +5,7 @@ const primaryColor = Color(0xFFF75F55);
 const hintColor = Color(0xFFA4A9AF);
 const iconColor = Color(0xFF7C8592);
 final shadowOneColor = const Color(0xFF395AB8).withOpacity(.10);
+final shadowTwoColor = const Color(0xFF395AB8).withOpacity(.10);
 
 const gradientOne = LinearGradient(
   colors: [
@@ -13,4 +14,11 @@ const gradientOne = LinearGradient(
   ],
   begin: Alignment.topLeft,
   end: Alignment.bottomRight,
+);
+
+final boxShadowOne =  BoxShadow(
+  color: Colors.grey.withOpacity(0.2),
+  spreadRadius: 3,
+  blurRadius: 5,
+  offset: const Offset(4, 4), // changes position of shadow
 );
